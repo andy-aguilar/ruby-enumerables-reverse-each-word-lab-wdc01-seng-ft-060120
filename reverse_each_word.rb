5964ad34ff24cc0e36d0db3508d942b4ad22746a
Andres Aguilar <aaguil3@gmail.com>
2020-05-19 19:17:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,9 @@
 def reverse_each_word (string)
   word_split = string.split (" ")
   
-  word_split.collect! do |array|
+  word_split.each do |array|
   array.split ("")
   end
   
-  word_split.collect do |array|
-  array.reverse_each
-  end
+
 end
